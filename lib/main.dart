e41d3086f4f84a4e5037b5185cc57f8042d448c2
Varun Resh Maddali <mvresh@gmail.com>
2019-10-01 10:55:44 +0530

    containers, axisalignments, insets and adding image into containers and margins and sizedboxes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
+  Container container1 = Container(
+    width: 500,
+    height: 100,
+    color: Colors.green,
+  );
+
+  Container container2 = Container(
+    width: 500,
+    height: 100,
+    color: Colors.white,
+    child: Center(child: Image(image: NetworkImage('https://raw.githubusercontent.com/McLarenCollege/Flutter-Course-Notes/master/ensign.png'))),
+  );
+  Container container3 = Container(
+    width: 500,
+    height: 100,
+    color: Colors.orange,
+  );
+  List<Widget> containers = [container1,SizedBox(height: 20), container2,SizedBox(height: 20), container3];
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amberAccent,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: containers,
+          ),
+        ),
       ),
     ),
   );
